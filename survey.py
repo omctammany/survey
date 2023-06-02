0db4e5f349d5cc1b69daa82d6e8791490d97ea9f
omctammany <130871426+omctammany@users.noreply.github.com>
2023-06-02 13:42:26 -1000

    initial commit

---
+++ survey.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit_survey as ss
+import pandas as pd
+
+survey = ss.StreamlitSurvey()
+
+text = survey.text_input("Test text", id="ttt")
+survey.selectbox("Selection box:", options=["Option 1", "Option 2", "Option 3", "etc"])
+survey.multiselect("Multiple choice:", options=["Option 1", "Option 2", "Option 3", "etc"])
+survey.checkbox("Check box")
+survey.dateinput("Date input:")
+survey.timeinput("Time input:")
+survey.text_area("Area input:")
+st.number_input("Number input:", min_value=0, max_value=100, value=50)
+
+survey.to_json()
